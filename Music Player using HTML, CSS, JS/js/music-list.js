47903fbc9c1d972caab4eee0d4ad6971f09ccbe1
CSE <96760901+computerscienceengineers@users.noreply.github.com>
2021-12-31 12:32:02 +0530

    Add files via upload

---
+++ Music Player using HTML, CSS, JS/js/music-list.js
@@ -0,0 +1,48 @@
+let allMusic = [
+  {
+    name: "Tajdar e Haram",
+    artist: "Atif Aslam",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Suge",
+    artist: "Dababy",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Dillagi",
+    artist: "Rahat Fateh Ali Khan",
+    img: "music-3",
+    src: "music-3"
+  },
+  
+  {
+    name: "Chori Chori",
+    artist: "Sunanda Sharma",
+    img: "music-4",
+    src: "music-4"
+  },
+{
+    name: "Baarish",
+    artist: "Stebin Ben x Payal Dev",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Maastani",
+    artist: "B Praak x Jaani",
+    img: "music-6",
+    src: "music-6"
+  },
+
+{
+    name: "Dil Tutda",
+    artist: "Jassi Gill",
+    img: "music-7",
+    src: "music-7"
+  }
+
+ 
+];
